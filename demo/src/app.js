7566cd65cb7292251c1bc235bcca0eb0f2e080f8
stoffeastrom <stoffeastrom@gmail.com>
2016-03-27 20:41:38 +0200

    chore(demo): fix spinner

--- demo/src/app.js
+++ demo/src/app.js
@@ -1,7 +1,7 @@
 export class App {
   configureRouter(config, router) {
     config.title = "Sortable";
-    //config.options.pushState = true; TODO:
+    //config.options.pushState = true;
     config.map([
       { route: ["", "hub"], name: "hub", moduleId: "hub", nav: false, title: "Hub" }
     ]);
